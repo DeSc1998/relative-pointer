7177429b4b47051614f5673f8efdd64bb0eb0b9b
Dennis Schulze <dennis@axelschulze.net>
2022-08-15 21:18:01 +0200

    applied formatting

--- include/relative_ptr.hpp
+++ include/relative_ptr.hpp
@@ -3,74 +3,76 @@
 #include <concepts>
 #include <limits>
 
-template <typename Type, std::signed_integral Size = int> class relative_ptr {
-  using size_type = Size;
+template < typename Type, std::signed_integral Size = int >
+class relative_ptr {
+  using size_type  = Size;
   using value_type = Type;
 
   size_type ptr = default_null;
 
 protected:
-  static constexpr size_type default_null =
-      std::numeric_limits<size_type>::min();
+  static constexpr size_type default_null
+    = std::numeric_limits< size_type >::min( );
 
-  virtual size_type encode(size_type raw) const { return raw ^ default_null; }
+  virtual size_type encode( size_type raw ) const { return raw ^ default_null; }
 
-  virtual size_type decode(size_type raw) const { return raw ^ default_null; }
+  virtual size_type decode( size_type raw ) const { return raw ^ default_null; }
 
 public:
-  relative_ptr(const value_type *ptr) {
-    const char *rel = reinterpret_cast<char *>(&this->ptr);
-    const char *abs = reinterpret_cast<const char *>(ptr);
-    this->ptr = encode(static_cast<size_type>(abs - rel));
+  relative_ptr( const value_type* ptr ) {
+    const char* rel = reinterpret_cast< char* >( &this->ptr );
+    const char* abs = reinterpret_cast< const char* >( ptr );
+    this->ptr       = encode( static_cast< size_type >( abs - rel ) );
   }
 
-  relative_ptr &operator=(const value_type *ptr) {
-    const char *rel = reinterpret_cast<char *>(&this->ptr);
-    const char *abs = reinterpret_cast<const char *>(ptr);
-    this->ptr = encode(static_cast<size_type>(abs - rel));
+  relative_ptr& operator=( const value_type* ptr ) {
+    const char* rel = reinterpret_cast< char* >( &this->ptr );
+    const char* abs = reinterpret_cast< const char* >( ptr );
+    this->ptr       = encode( static_cast< size_type >( abs - rel ) );
     return *this;
   }
 
-  operator bool() const { return decode(ptr) != 0; }
+  operator bool( ) const { return decode( ptr ) != 0; }
 
-  relative_ptr() = default;
-  relative_ptr(const relative_ptr &) = default;
-  relative_ptr(relative_ptr &&) = default;
+  relative_ptr( )                     = default;
+  relative_ptr( const relative_ptr& ) = default;
+  relative_ptr( relative_ptr&& )      = default;
 
-  relative_ptr &operator=(const relative_ptr &) = default;
-  relative_ptr &operator=(relative_ptr &&) = default;
-
-  // explicit casting needed otherwise implicit cast to Node* would favored over
-  // cast to bool for some reason
-  explicit operator const value_type *() const {
-    return reinterpret_cast<const value_type *>(
-        reinterpret_cast<const char *>(&ptr) + decode(ptr));
-  }
+  relative_ptr& operator=( const relative_ptr& ) = default;
+  relative_ptr& operator=( relative_ptr&& )      = default;
 
   // explicit casting needed otherwise implicit cast to Node* would favored over
   // cast to bool for some reason
-  explicit operator value_type *() {
-    return reinterpret_cast<value_type *>(reinterpret_cast<char *>(&ptr) +
-                                          decode(ptr));
+  explicit operator const value_type*( ) const {
+    return reinterpret_cast< const value_type* >(
+      reinterpret_cast< const char* >( &ptr ) + decode( ptr ) );
   }
 
-  value_type *operator->() const {
-    return reinterpret_cast<const value_type *>(
-        reinterpret_cast<const char *>(&ptr) + decode(ptr));
+  // explicit casting needed otherwise implicit cast to Node* would favored over
+  // cast to bool for some reason
+  explicit operator value_type*( ) {
+    return reinterpret_cast< value_type* >( reinterpret_cast< char* >( &ptr )
+                                            + decode( ptr ) );
   }
 
-  value_type &operator*() {
-    const char *tmp = reinterpret_cast<char *>(&ptr);
-    const value_type *loc = reinterpret_cast<value_type *>(tmp + decode(ptr));
+  value_type* operator->( ) const {
+    return reinterpret_cast< const value_type* >(
+      reinterpret_cast< const char* >( &ptr ) + decode( ptr ) );
+  }
+
+  value_type& operator*( ) {
+    const char*       tmp = reinterpret_cast< char* >( &ptr );
+    const value_type* loc
+      = reinterpret_cast< value_type* >( tmp + decode( ptr ) );
     return *loc;
   }
 
-  const value_type &operator*() const {
-    const char *const tmp = reinterpret_cast<const char *>(&ptr);
-    const value_type *const loc =
-        reinterpret_cast<const value_type *>(tmp + decode(ptr));
+  const value_type& operator*( ) const {
+    const char* const       tmp = reinterpret_cast< const char* >( &ptr );
+    const value_type* const loc
+      = reinterpret_cast< const value_type* >( tmp + decode( ptr ) );
     return *loc;
   }
 
-  virtual ~relative_ptr() {}
+  virtual ~relative_ptr( ) { }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,39 +5,39 @@
 #include "relative_ptr.hpp"
 
 struct Node {
-  int value;
-  relative_ptr<Node> left, right;
+  int                  value;
+  relative_ptr< Node > left, right;
 };
 
-void print_tree(Node *root, size_t indent_level = 0) {
-  for (size_t i = 0; i < indent_level; ++i) {
-    printf("  ");
+void print_tree( Node* root, size_t indent_level = 0 ) {
+  for ( size_t i = 0; i < indent_level; ++i ) {
+    printf( "  " );
   }
-  printf("%i\n", root->value);
-  if (root->left)
+  printf( "%i\n", root->value );
+  if ( root->left )
     // explicit casting needed (see 'include/relative_ptr.hpp' for reason)
-    print_tree(static_cast<Node *>(root->left), indent_level + 1);
-  if (root->right)
+    print_tree( static_cast< Node* >( root->left ), indent_level + 1 );
+  if ( root->right )
     // explicit casting needed
-    print_tree(static_cast<Node *>(root->right), indent_level + 1);
+    print_tree( static_cast< Node* >( root->right ), indent_level + 1 );
 }
 
-auto main() -> int {
-  std::vector<Node> vec = {};
+auto main( ) -> int {
+  std::vector< Node > vec = { };
 
-  vec.resize(3);
+  vec.resize( 3 );
   vec[0].value = 0;
   vec[1].value = 2;
   vec[2].value = 1;
 
-  vec[0].left = &vec[2];
+  vec[0].left  = &vec[2];
   vec[0].right = &vec[1];
 
-  printf("before resize:\n");
-  print_tree(&vec[0]);
+  printf( "before resize:\n" );
+  print_tree( &vec[0] );
 
-  vec.resize(10);
+  vec.resize( 10 );
 
-  printf("after resize:\n");
-  print_tree(&vec[0]);
+  printf( "after resize:\n" );
+  print_tree( &vec[0] );
 }

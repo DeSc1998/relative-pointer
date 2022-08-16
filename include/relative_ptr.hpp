227c56cb0898e039dc447469417e9de4e13582b9
Dennis Schulze <dennis@axelschulze.net>
2022-08-16 17:53:27 +0200

    cleanup + removed some code duplication

--- include/relative_ptr.hpp
+++ include/relative_ptr.hpp
@@ -3,41 +3,43 @@
 #include <concepts>
 #include <limits>
 
-template < typename Type, std::signed_integral Size = int >
+template < typename Type, std::signed_integral Size = long >
 class relative_ptr {
   using size_type  = Size;
   using value_type = Type;
 
-  size_type ptr = default_null;
-
-protected:
   static constexpr size_type default_null
     = std::numeric_limits< size_type >::min( );
 
-  virtual size_type encode( size_type raw ) const { return raw ^ default_null; }
+  size_type ptr = default_null;
 
-  virtual size_type decode( size_type raw ) const { return raw ^ default_null; }
+  static size_type encode( size_type raw ) { return raw ^ default_null; }
+  static size_type decode( size_type raw ) { return raw ^ default_null; }
 
-public:
-  relative_ptr( const value_type* ptr ) {
-    const char* rel = reinterpret_cast< char* >( &this->ptr );
+  static size_type ptr_diff( const value_type* ptr, const size_type& dest ) {
+    const char* rel = reinterpret_cast< const char* >( &dest );
     const char* abs = reinterpret_cast< const char* >( ptr );
-    this->ptr       = encode( static_cast< size_type >( abs - rel ) );
+    return static_cast< size_type >( abs - rel );
   }
 
+public:
   relative_ptr( )                     = default;
   relative_ptr( const relative_ptr& ) = default;
   relative_ptr( relative_ptr&& )      = default;
 
-  relative_ptr& operator=( const value_type* ptr ) {
-    const char* rel = reinterpret_cast< char* >( &this->ptr );
-    const char* abs = reinterpret_cast< const char* >( ptr );
-    this->ptr       = encode( static_cast< size_type >( abs - rel ) );
-    return *this;
-  }
+  relative_ptr( const size_type& rel )
+    : ptr( rel ) { }
+
+  relative_ptr( const value_type* ptr )
+    : ptr( encode( ptr_diff( ptr, this->ptr ) ) ) { }
 
   relative_ptr& operator=( const relative_ptr& ) = default;
   relative_ptr& operator=( relative_ptr&& )      = default;
+
+  relative_ptr& operator=( const value_type* ptr ) {
+    this->ptr = encode( ptr_diff( ptr, this->ptr ) );
+    return *this;
+  }
 
   operator bool( ) const { return decode( ptr ) != 0; }
   operator bool( ) { return decode( ptr ) != 0; }
@@ -52,29 +54,12 @@ public:
                                             + decode( ptr ) );
   }
 
-  const value_type* operator->( ) const {
-    return reinterpret_cast< const value_type* >(
-      reinterpret_cast< const char* >( &ptr ) + decode( ptr ) );
-  }
+  value_type*       operator->( ) { return *this; }
+  const value_type* operator->( ) const { return *this; }
 
-  value_type* operator->( ) {
-    return reinterpret_cast< value_type* >( reinterpret_cast< char* >( &ptr )
-                                            + decode( ptr ) );
-  }
-
-  value_type& operator*( ) {
-    const char*       tmp = reinterpret_cast< char* >( &ptr );
-    const value_type* loc
-      = reinterpret_cast< value_type* >( tmp + decode( ptr ) );
-    return *loc;
-  }
+  value_type& operator*( ) { return *static_cast< value_type* >( *this ); }
 
   const value_type& operator*( ) const {
-    const char* const       tmp = reinterpret_cast< const char* >( &ptr );
-    const value_type* const loc
-      = reinterpret_cast< const value_type* >( tmp + decode( ptr ) );
-    return *loc;
+    return *static_cast< const value_type* >( *this );
   }
-
-  virtual ~relative_ptr( ) { }
 };
